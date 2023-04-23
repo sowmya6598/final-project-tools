1e79f81cc74a8e6c5d409c7f0942164784f6e7d5
Ishaan Vaish <ishaanvaish@gmail.com>
2023-04-22 22:37:24 -0400

    Fix query 2

--- 2_day_week.sql
+++ 2_day_week.sql
@@ -1,5 +1,5 @@
 SELECT 
-    CASE strftime('%w', pickup_datetime)
+    CASE strftime('%w', pickup_time)
         WHEN '0' THEN 'Sunday'
         WHEN '1' THEN 'Monday'
         WHEN '2' THEN 'Tuesday'
@@ -10,10 +10,9 @@ SELECT
     END AS day_of_week, 
     COUNT(*) AS frequency
 FROM UBER_TRIPS
-WHERE pickup_datetime BETWEEN '2009-01-01 00:00:00 UTC' AND '2015-06-30 23:59:59 UTC' 
-AND pickup_datetime IS NOT NULL
+WHERE pickup_time BETWEEN '2009-01-01 00:00:00 UTC' AND '2015-06-30 23:59:59 UTC'
 GROUP BY 
-    CASE strftime('%w', pickup_datetime)
+    CASE strftime('%w', pickup_time)
         WHEN '0' THEN 'Sunday'
         WHEN '1' THEN 'Monday'
         WHEN '2' THEN 'Tuesday'
@@ -22,4 +21,4 @@ GROUP BY
         WHEN '5' THEN 'Friday'
         WHEN '6' THEN 'Saturday'
     END
-ORDER BY CAST(strftime('%w', pickup_datetime) AS INTEGER);
+ORDER BY CAST(strftime('%w', pickup_time) AS INTEGER);
